bcbd2a2b799a16d008fc458b153127c28a83cfaf
임아영 <96092286+dladkdud@users.noreply.github.com>
2021-12-20 12:16:42 +0900

    Update q029.js

--- dladkdud_js/q029/q029.js
+++ dladkdud_js/q029/q029.js
@@ -1,22 +1,75 @@
-let player = prompt("가위 바위 보 입력: ");
-var com = Math.floor(Math.random() * 3);
+var player = "";
 
-if (player=="가위" && com==0 ) {
-    dw ("draw!!");
-} else if (player=="가위" && com==1 ) {
-    dw("computer win!!");
-} else if (player=="가위" && com==2 ) {
-    dw ("player win!!");
-} else if (player=="바위" && com==0 ) {
-    dw ("player win!!");
-} else if (player=="바위" && com==1 ) {
-    dw ("draw!!");
-} else if (player=="바위" && com==2 ) {
-    dw ("computer win!!");
-} else if (player=="보" && com==0 ) {
-    dw ("computer win!!!!");
-} else if (player=="보" && com==1 ) {
-    dw ("player win!!");
-} else if (player=="보" && com==2 ) {
-    dw ("draw!!");
+player = prompt ("가위 바위 보 입력");
+
+while (true){
+    if (player == "가위" || player == "바위" || player == "보" ) {
+        break; 
+    } else {
+        alert ("다시 입력해주세요.")
+    }
 }
+
+var com = Math.floor(Math.random() * 3 );
+
+
+if ( com == 0 ) {
+    com = "가위" ;
+}
+if ( com == 1 ) {
+    com = "바위" ;
+}
+if ( com == 2 ) {
+    com = "보" ;
+}
+
+dw ( "유저: " + player );
+br();
+dw ( "컴퓨터: " + com );
+br();
+
+var win = "" ; 
+
+switch (player) {
+    case "가위" :
+        switch (com) {
+            case "가위" :
+                win = "draw!!";
+                break;
+            case "바위" : 
+                win = "com win!!";
+                break;
+            case "보" : 
+                win = "you win!!";
+                break;
+        }
+        break;
+    case "바위" :
+        switch (com) {
+            case "가위" : 
+                win = "you win!!!!";
+                break;
+            case "바위" : 
+                win = "draw!!";
+                break;
+            case "보" : 
+                win = "com win!!";
+                break;
+        }
+        break;
+    case "보" :
+        switch (com) {
+            case "가위" : 
+                win = "com win!!";
+                break;
+            case "바위" : 
+                win = "you win!!";
+                break;
+            case "보" : 
+                win = "draw!!";
+                break;
+        }
+        break; 
+}
+
+dw (win); 
