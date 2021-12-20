ff36eabc0d8b71c2ff02f682a23ce10f7d19c028
dladkdud <96092286+dladkdud@users.noreply.github.com>
2021-12-20 10:17:53 +0900

    Update q029.js

--- dladkdud_js/q029/q029.js
+++ dladkdud_js/q029/q029.js
@@ -1,18 +1,22 @@
-let player = prompt("가위(0) 바위(1) 보(2) 입력: ");
+let player = prompt("가위 바위 보 입력: ");
 var com = Math.floor(Math.random() * 3);
 
-if (player==com) {
-    dw("draw")
-} else if (player=="0" && com==1 ) {
+if (player=="가위" && com==0 ) {
+    dw ("draw!!");
+} else if (player=="가위" && com==1 ) {
     dw("computer win!!");
-} else if (player=="0" && com==2 ) {
+} else if (player=="가위" && com==2 ) {
     dw ("player win!!");
-} else if (player=="1" && com==0 ) {
+} else if (player=="바위" && com==0 ) {
     dw ("player win!!");
-} else if (player==1 && com==2 ) {
+} else if (player=="바위" && com==1 ) {
+    dw ("draw!!");
+} else if (player=="바위" && com==2 ) {
     dw ("computer win!!");
-} else if (player==2 && com==0 ) {
+} else if (player=="보" && com==0 ) {
     dw ("computer win!!!!");
-} else if (player==2 && com==1 ) {
+} else if (player=="보" && com==1 ) {
     dw ("player win!!");
-} 
+} else if (player=="보" && com==2 ) {
+    dw ("draw!!");
+}
